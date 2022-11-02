fd8716cc81b5503c6bddf918cb705de8fb7fe641
NikolayLeshchev <kolyaleshiov@gmail.com>
2022-11-02 17:39:17 +0300

    create project and build structure of app for layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Heder(){
+  return(<header className="header">
+  <img
+    src="https://www.freelogovectors.net/wp-content/uploads/2020/03/harley-davidson-logo.png"
+    alt="logo"
+  />
+</header>)
+}
+
+
+
+
+export default Heder;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+function Navbar(){
+  return(      <nav className="nav">
+        <div>
+          <a href="#p">Profile</a>
+        </div>
+        <div>
+          <a href="#m">Messages</a>
+        </div>
+        <div>
+          <a href="#n">News</a>
+        </div>
+        <div>
+          <a href="#m">Music</a>
+        </div>
+
+        <div>
+          <a href="#s">Settings</a>
+        </div>
+      </nav>)
+}
+
+
+
+
+export default Navbar;
